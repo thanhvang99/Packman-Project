b199a0de11e730ca7366c6f57c092c40b69a9d68
thuantran664488 <trandinhthuan0112@gmail.com>
2019-06-08 10:26:35 +0700

    86%

--- PacMan-Project/src/App/Main.java
+++ PacMan-Project/src/App/Main.java
@@ -1,12 +1,9 @@
 package App;
 
 
-import Controller.GhostController;
-import Controller.GraphController;
 import Controller.MenuStateController;
-import Controller.PacController;
-import Entity.*;
-import UI.UIGame;
+import Entity.GameObject;
+import Entity.State;
 import UI.UIMenu;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
@@ -14,8 +11,6 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 import org.newdawn.slick.TrueTypeFont;
-
-import java.util.ArrayList;
 
 import static org.lwjgl.opengl.GL11.*;
 
@@ -56,34 +51,34 @@ public class Main {
         UIMenu uiMenu = new UIMenu(1,"menu",1,"jpg");
         MenuStateController menuStateController = new MenuStateController(uiMenu);
 
-        ArrayList<Ghost> listOfGhosts = new ArrayList<Ghost>();
-
-        Entity pac = new PacMan(3,3,"pac",11,22,"gif");
-        Entity ghost = new Ghost(0,4,"ghost",3,3,"gif");
-        Entity ghost1 = new Ghost(0,16,"ghost",3,3,"gif");
-        Entity ghost2 = new Ghost(21,16,"ghost",3,3,"gif");
-        Entity ghost3 = new Ghost(19,3,"ghost",3,3,"gif");
-        pac.setSpeed(2);
-        ghost.setSpeed(1);
-        ghost1.setSpeed(1);
-
-
-
-
-        listOfGhosts.add((Ghost)ghost);
-//        listOfGhosts.add((Ghost)ghost1);
-//        listOfGhosts.add((Ghost)ghost2);
-//        listOfGhosts.add((Ghost)ghost3);
-
-        Graph graph = new Graph("maze.txt");
-
-
-
-
-        new UIGame(pac,listOfGhosts,graph);
-        new PacController(pac);
-        new GraphController(graph,listOfGhosts,pac);
-        new GhostController(listOfGhosts,graph);
+//        ArrayList<Ghost> listOfGhosts = new ArrayList<Ghost>();
+//
+//        Entity pac = new PacMan(3,3,"pac",11,22,"gif");
+//        Entity ghost = new Ghost(0,4,"ghost",3,3,"gif");
+//        Entity ghost1 = new Ghost(0,16,"ghost",3,3,"gif");
+//        Entity ghost2 = new Ghost(21,16,"ghost",3,3,"gif");
+//        Entity ghost3 = new Ghost(19,3,"ghost",3,3,"gif");
+//        pac.setSpeed(2);
+//        ghost.setSpeed(1);
+//        ghost1.setSpeed(1);
+//
+//
+//
+//
+//        listOfGhosts.add((Ghost)ghost);
+////        listOfGhosts.add((Ghost)ghost1);
+////        listOfGhosts.add((Ghost)ghost2);
+////        listOfGhosts.add((Ghost)ghost3);
+//
+//        Graph graph = new Graph("maze.txt");
+//
+//
+//
+//
+//        new UIGame(pac,listOfGhosts,graph);
+//        new PacController(pac);
+//        new GraphController(graph,listOfGhosts,pac);
+//        new GhostController(listOfGhosts,graph);
 
     }
     private static void setUpWindow(){
--- PacMan-Project/src/Controller/GameStateController.java
+++ PacMan-Project/src/Controller/GameStateController.java
@@ -1,26 +1,27 @@
 package Controller;
 
+import Entity.State;
 import UI.UIGame;
 
-public class GameStateController {
+public class GameStateController extends StateController {
 
     private UIGame uiEntity;
 
     public GameStateController(UIGame uiEntiity) {
-//        super(uiEntiity);
-//        setCurrentController(this);
-//
-//    }
-//
-//    @Override
-//    public void updateState() {
-//
-//    }
-//
-//    @Override
-//    public boolean checkInput() {
-//        return false;
-//    }
+        super(uiEntiity);
+        this.uiEntity = uiEntiity;
+        State.setCurrentStateController(this);
 
     }
+
+    @Override
+    public void updateState() {
+
+    }
+
+    @Override
+    public boolean checkInput() {
+        return false;
+    }
+
 }
